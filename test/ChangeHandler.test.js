359e45a0336340f19e8505a1f6079c6d5fdefcd7
luykl <leah.logsin@gmail.com>
2021-04-28 15:40:21 -0400

    constructor working

--- test/ChangeHandler.test.js
+++ test/ChangeHandler.test.js
@@ -7,6 +7,27 @@ describe("ChangeHandler", function() {
   });
 
   // TODO: Add additional tests below...
+  //CONSTRUCTOR
+  test("amountDue set with argument", function() {
+    const ch = new ChangeHandler(5)
+    expect(ch.amountDue).toBe(5);
+  });
+
+  test("different amountDue set with argument", function() {
+    const ch = new ChangeHandler(165)
+    expect(ch.amountDue).toBe(165);
+  });
+
+  test("0 amountDue set with argument", function() {
+    const ch = new ChangeHandler(0)
+    expect(ch.amountDue).toBe(0);
+  });
+
+  test("cashTendered set to zero", function() {
+    const ch = new ChangeHandler(7)
+    expect(ch.cashTendered).toBe(0);  
+  });
+
   
   
   
